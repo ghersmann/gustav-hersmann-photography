7f3d6b5faa5db61528b30f9125e708c692dd7e5a
Gustav Hersmann <gustav@gustavhersmann.com>
2024-06-22 10:24:19 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gustav-hersmann-photography
+
+I used to be a professional photographer. Here is my portfolio for my former business. If you need a photographer for a project, send me an email: gustav@gustavhersmann.com
---
+++ src/views/MainView.vue
@@ -0,0 +1,17 @@
+<template>
+  <body>
+    <header>
+      <h1>Gustav Hersmann Photographer</h1>
+    </header>
+    <main class="main-c">
+      <img
+        src="../assets/img/2013-hochzillertal-kaltenbach-1128.jpg"
+        alt="Snowboarder at Zillertal Välley Rälley in 2013"
+        width="700px"
+      />
+    </main>
+    <footer>
+      <h2><a href="mailto:gustav@gustavhersmman.com">gustav@gustavhersmann.com</a></h2>
+    </footer>
+  </body>
+</template>
